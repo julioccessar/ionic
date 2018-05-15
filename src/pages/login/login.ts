d19e8d9c650450746dc3d7066f36d1ff3defcd27
Ariel <ari.retamar@gmail.com>
2018-05-15 19:34:29 -0300

    Saca modo iOS por default de app.module.ts. Añade validación de campos para login.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 
 @IonicPage({
   name: 'login'
@@ -9,12 +9,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
+  public datosLogin: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public toastCtrl: ToastController,
+  ) {
+    this.datosLogin = {
+      username: '',
+      password: '',
+    };
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
+  public login(): void {
+    if (this.datosLogin.username.length < 3 || this.datosLogin.password.length < 3) {
+      let modalError = this.toastCtrl.create({
+        duration: 2500,
+        message: 'Campos incompletos, por favor complete todos los campos.',
+        position: 'bottom'
+      });
+
+      modalError.present();
+    }
+  }
 }
